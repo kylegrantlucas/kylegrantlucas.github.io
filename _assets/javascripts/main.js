cdb48fa9debd7127681f5ce25caec217994957d9
Kyle Lucas <kglucas93@gmail.com>
2015-05-22 19:39:38 -0700

    Way refactor half of everything

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
   $('section:last').addClass('subtle-dropshadow');
   $("header").headroom({
-  "offset": 205,
-  "tolerance": 5,
-  "classes": {
-    "initial": "animated",
-    "pinned": "slideDown",
-    "unpinned": "slideUp"
-  }
-});
+    "offset": 205,
+    "tolerance": 5,
+    "classes": {
+      "initial": "animated",
+      "pinned": "slideDown",
+      "unpinned": "slideUp"
+    }
+  });
+  $($('.side-nav ul a')[0]).addClass('first-dot');
+  $($('.side-nav ul a')[0]).addClass('active');
   $('.dot-nav a').on('click', function() {
-    console.log(this)
     var scrollAnchor = $(this).attr('scroll-data'),
     scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top;
-    console.log('evaluation...')
     $('body').stop().animate({
       scrollTop: scrollPoint
     }, 1000);
     return false;
   })
 
-  $('#toTop #top').click(function () {
+  $('#to-top').click(function () {
     $('body').animate({
       scrollTop: 0
     }, 1000);
@@ -34,16 +34,10 @@ $(document).ready(function() {
       var currLink = $(this);
       var scrollAnchor = $(this).attr('scroll-data');
       var refElement = $('section[data-anchor="' + scrollAnchor + '"]');
-
-
-
-
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-          $('.dot-nav a.active').removeClass("active");
-          currLink.addClass("active");
-        } 
-
-      
+      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+        $('.dot-nav a.active').removeClass("active");
+        currLink.addClass("active");
+      } 
     });
   });
 });
