e8fb4e71612805d15d3d00cccb155e6c56c3b3fe
Kyle Lucas <kyle@kylelucas.io>
2019-03-25 20:22:40 -0700

    Update terminal, use react-fish, update content

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Terminal from 'react-terminal-emulator';
-import logo from './logo.svg';
+import Terminal from 'react-fish';
+import renderHTML from 'react-render-html';
 import './App.css';
-import { strictEqual } from 'assert';
+// import { strictEqual } from 'assert';
 
 export const clear = {
     exec: ({ structure, history, cwd }, command) => {
@@ -23,7 +23,7 @@ class App extends Component {
 }
 
 const styles = {
-  ReactBash: {
+  ReactFish: {
     borderRadius: '5px',
     display: 'flex',
     flexDirection: 'column',
@@ -39,39 +39,38 @@ const styles = {
 const theme = 'solarized'
 const prefix = 'kyle@kylelucas.io';
 const history = [
-  { value: 'Welcome to my terminal!' },
-  { value: ' ' },
-  { value: 'My name is Kyle Lucas and i\'m a Software Engineer from Los Angeles, California.' },
-  { value: 'If you\'d like to learn more about me go ahead and run `cat README.md` without the backticks :)' }
+  { value: renderHTML('Welcome to my terminal!<br><br>My name is Kyle Lucas and i\'m a Software Engineer from Los Angeles, California.<br><br>If you\'d like to learn more about me go ahead and run `cat README.md` without the backticks :)') },
 ];
 
 const structure = {
-  '.hiddenFile': { content: 'This is a hidden file' },
-  "CREDITS.md": { content: 'First thanks to /u/Flurokazoo and the awesome Last.FM collage generator @ http://background.jasperdekroon.nl' },
+  "CREDITS.md": { content: 'First thanks to /u/Flurokazoo and the awesome <a href="http://background.jasperdekroon.nl">Last.FM collage generator</a>' },
   'README.md': { content: '# Kyle Lucas' },
   projects: {
-    c: {
-      sugarkorn: {
-        content: 'This is the text content for <file1> of <src>'
-      },
-    },
+    // c: {
+    //   sugarkorn: {
+    //     content: 'This is the text content for <file1> of <src>'
+    //   },
+    // },
     go: {
       'plex-lametric.md': {
-        content: '# plex-lametric https://github.com/kylegrantlucas/plex-lametric'
+        content: '# <a href="https://github.com/kylegrantlucas/plex-lametric">plex-lametric</a><br><br>A small little HTTP server that listens to a Plex Media Server websocket to display Now Playing information on a LaMetric clock.'
       },
       'transmission-exporter.md': {
-        content: '# transmission-explorer https://github.com/kylegrantlucas/transmission-explorer'
+        content: '# <a href="https://github.com/kylegrantlucas/transmission-explorer">transmission-explorer</a><br><br>A prometheus exporter for Transmission metrics.'
       }
     },
     js: {
       'kylelucas-io.md': {
-        content: 'This is the text content for <file1> of <src>'
+        content: '# <a href="https://github.com/kylegrantlucas/kylelucas.io">kylelucas.io</a><br><br>This website! Build entirely with TypeScript using React.'
+      },
+      'react-fish': {
+        content: '# <a href="https://github.com/kylegrantlucas/react-fish">react-fish</a><br><br>The terminal you\'re using! It\'s my first NPM module and a fork of an existing project. I spruced it up a little bit, and gave it some new features!'
       },
     },
     other: {},
     ruby: {
       "singing_assistant.md": {
-        content: 'This is the text content for <file1> of <src>'
+        content: '# <a href="https://github.com/kylegrantlucas/singing-assistan">transmission-explorer</a><br><br>A prometheus exporter for Transmission metrics.'
       },
     },
   },
