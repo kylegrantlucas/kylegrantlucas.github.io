88b35b0a346ea00cb0b9dc4d151a1c094069098f
Kyle Lucas <kyle@kylelucas.io>
2021-09-28 23:41:26 -0700

    Clean up refactor start

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function(config) {
+    config.addPassthroughCopy({ public: './' })
+  
+    config.setBrowserSyncConfig({
+      files: ['dist/**/*'],
+    })
+  
+    return {
+      templateFormats: ['md', 'njk', 'jpg', 'png', 'gif'],
+      dir: {
+        input: 'src',
+        output: '_site',
+        layouts: '_js/layouts',
+      }
+    }
+  }
