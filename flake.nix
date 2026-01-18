1d7cabaabe7ca36e062963bbe6002673ba88e35a
Kyle Lucas <kyle@kylelucas.io>
2026-01-17 20:44:01 -0800

    add flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "kylelucas.io - personal site with Fastly Compute backend";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22      # Node.js 22 LTS
+            fastly         # Fastly CLI for Compute deployment
+          ];
+
+          shellHook = ''
+            echo "kylelucas.io dev environment"
+            echo "  node: $(node --version)"
+            echo "  fastly: $(fastly version)"
+          '';
+        };
+      }
+    );
+}
